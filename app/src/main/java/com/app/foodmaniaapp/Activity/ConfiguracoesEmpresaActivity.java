72afb3fb1ab416de4d6bf56466192674944eb99d
rubenssi <rubenssi825@gmail.com>
2021-05-16 23:09:38 -0300

    add a opicao sair no menu empresa

---
+++ app/src/main/java/com/app/foodmaniaapp/Activity/ConfiguracoesEmpresaActivity.java
@@ -0,0 +1,14 @@
+package com.app.foodmaniaapp.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConfiguracoesEmpresaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_configuracoes_empresa);
+    }
+}
--- app/src/main/java/com/app/foodmaniaapp/Activity/EmpresaActivity.java
+++ app/src/main/java/com/app/foodmaniaapp/Activity/EmpresaActivity.java
@@ -5,22 +5,28 @@ import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
+import com.app.foodmaniaapp.Helper.FirebaseConfig;
 import com.app.foodmaniaapp.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class EmpresaActivity extends AppCompatActivity {
+
+    private FirebaseAuth autentication;
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_empresa);
+
+        autentication = FirebaseConfig.getFirefebaseAutentication();
 
         // Configuração Toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -41,9 +47,32 @@ public class EmpresaActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 
         switch (item.getItemId()) {
-
+            case R.id.menuSair :
+                deslogarUsuario();
+                break;
+            case R.id.menuConfiguracoes :
+                //abrirConfigaracoes();
+                break;
+            case R.id.menuNovoProduto :
+                //abrirNovoProduto();
+                break;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void deslogarUsuario() {
+        try {
+
+            autentication.signOut();
+
+            
+            Intent intent = new Intent(EmpresaActivity.this, AutenticationActivity.class);
+            startActivity(intent);
+            finish();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
